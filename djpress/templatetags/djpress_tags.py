06ae064d9914474ce738b717d9738606934a32dd
Stuart Maxwell <stuart@amanzi.nz>
2024-05-01 15:49:50 +1200

    Update blog title in settings and templates

--- djpress/templatetags/djpress_tags.py
+++ djpress/templatetags/djpress_tags.py
@@ -3,6 +3,7 @@
 from django import template
 from django.db import models
 
+from config.settings import BLOG_TITLE
 from djpress.models import Category, Content
 
 register = template.Library()
@@ -24,3 +25,9 @@ def get_recent_published_content() -> models.QuerySet[Category] | None:
 def get_single_published_content(slug: str) -> Content | None:
     """Return a single published post by slug."""
     return Content.post_objects.get_published_post_by_slug(slug)
+
+
+@register.simple_tag
+def get_blog_title() -> str:
+    """Return the blog title."""
+    return BLOG_TITLE
