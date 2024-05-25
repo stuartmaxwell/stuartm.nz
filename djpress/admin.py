86c6ca3c4e8575e4e94103c85da5663460aeff02
Stuart Maxwell <stuart@amanzi.nz>
2024-05-25 18:55:07 +1200

    Remove djpress package directory

--- djpress/admin.py
+++
@@ -1,9 +0,0 @@
-"""djpress admin configuration."""
-
-from django.contrib import admin
-
-# Register the models here.
-from djpress.models import Category, Post
-
-admin.site.register(Category)
-admin.site.register(Post)
