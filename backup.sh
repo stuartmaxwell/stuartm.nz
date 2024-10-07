8954869a07c2017c854f594409383708e0c4f393
Stuart Maxwell <stuart@amanzi.nz>
2024-10-08 02:28:24 +1300

    Fix S3 endpoint and tweak backup script

--- backup.sh
+++ backup.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
-#
+
 # Run using cron to backup db every hour
-#
+
 set -euf -o pipefail
 
 DB_PATH="/app/$DB_NAME.sqlite3"
 BACKUP_PATH="/app/backup"
-BACKUP_FILE="${BACKUP_PATH}/backup-$(date +%H).sqlite3"
-TAR_FILE="${BACKUP_PATH}/backup-$(date +%H).tar.gz"
+HOUR=$(date +%H)
+BACKUP_FILE="${BACKUP_PATH}/backup-${HOUR}.sqlite3"
+TAR_FILE="${BACKUP_PATH}/backup-${HOUR}.tar.gz"
 
 # Ensure the backup directory exists
 mkdir -p "${BACKUP_PATH}"
@@ -16,7 +17,8 @@ mkdir -p "${BACKUP_PATH}"
 sqlite3 "${DB_PATH}" "VACUUM INTO '${BACKUP_FILE}'"
 
 # Compress the backup
-tar --gzip -cf "${TAR_FILE}" "${BACKUP_FILE}"
+tar --gzip -cf "${TAR_FILE}" -C "${BACKUP_PATH}" "$(basename "${BACKUP_FILE}")"
+rm "${BACKUP_FILE}"
 
 # Upload to S3
-aws s3 cp "${TAR_FILE}" s3://$S3_BUCKET/backup-$(date +%H).tar.gz
+aws s3 cp "${TAR_FILE}" "s3://$S3_BUCKET/backup-${HOUR}.tar.gz" --endpoint-url $AWS_ENDPOINT_URL
