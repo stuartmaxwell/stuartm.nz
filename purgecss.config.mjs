ba272d836195b4ae0e8b94bec5afc3f952a3a05b
Stuart Maxwell <stuart@amanzi.nz>
2025-10-06 23:54:55 +1300

    Lots of UI updates and related stuff

---
+++ purgecss.config.mjs
@@ -0,0 +1,5 @@
+// https://github.com/FullHuman/purgecss/issues/1327#issuecomment-2963944182
+export const content = ['**/templates/**/*.html'];
+export const css = ['build/css/bootstrap.custom.css'];
+export const output = 'static/css/bs53p.css';
+export const safelist = ['form-control', 'show', 'modal-open'];
