ce4390da48e30b954bc821b202ddc846af7d42a7
Stuart Maxwell <stuart@amanzi.nz>
2025-12-04 18:31:16 +1300

    Remove default_auto_field from AppConfig

--- timezone_converter/apps.py
+++ timezone_converter/apps.py
@@ -6,5 +6,4 @@ from django.apps import AppConfig
 class TimezoneConverterConfig(AppConfig):
     """Timezone converter app configuration."""
 
-    default_auto_field = "django.db.models.BigAutoField"
     name = "timezone_converter"
